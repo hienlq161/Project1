23ec28cb6f8dcbf994affb2e7c058baaa1acb6fc
DUONG KHUONG DUY <92731328+JimmyMcGil1@users.noreply.github.com>
2023-03-11 17:43:31 +0700

    last update, remove lv

--- Project1/Assets/Scripts/UI/LevelSelectControl.cs
+++ Project1/Assets/Scripts/UI/LevelSelectControl.cs
@@ -6,29 +6,26 @@ public class LevelSelectControl : MonoBehaviour
 {
     public static LevelSelectControl instance { get; private set; }
     [SerializeField] List<GameObject> lv;
-    public static int lockLv = 2; 
+    public static int lockLv = 7; 
     private void Awake()
     {
         if (instance != null && instance != this) Destroy(this);
         else instance = this;
         
         //lock from lv lockLv
-        for (int i = lockLv; i <= lv.Count; i++)
+        for (int i = lockLv; i < lv.Count; i++)
         {
-            lv[i - 1].GetComponent<LevelSelect>().Lock();
+            lv[i].GetComponent<LevelSelect>().Lock();
         }
         for (int i = 1; i < lockLv; i++)
         {
             UnLockLevel(i);
         }
     }
-    private void Start()
-    {
-       
-    }
+   
     public void UnLockLevel(int x)
     {
-        if (lv.Count >= x) lv[x - 1].GetComponent<LevelSelect>().Unlock();
+        if (lv.Count >= x) lv[x].GetComponent<LevelSelect>().Unlock();
         else return;
     }
 
